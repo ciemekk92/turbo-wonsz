3cf3870a4f4e078ac0ba1113ebb345ca4b457b84
ciemekk92 <przemyslaw.reducha@gmail.com>
2021-11-12 21:12:24 +0100

    Remove build output, update gitignore

--- build/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Game_1 = require("./src/Game");
-var UI_1 = require("./src/UI");
-var game = new Game_1.Game(new UI_1.UI());
-game.start();
